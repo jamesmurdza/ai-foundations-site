7dc18cefcbb61bde4df93207e039e61e968a811d
James Murdza <james@jamesmurdza.com>
2025-04-10 15:29:53 +0200

    Remove pricing section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,6 @@ import { Features } from "@/components/features-section";
 import { FeaturesSection } from "@/components/features-section-2";
 import { Testimonials } from "@/components/testimonials";
 import { Faq } from "@/components/faq";
-import { Pricing } from "@/components/pricing";
 import { CtaSection } from "@/components/cta-section";
 import { Footer } from "@/components/footer";
 
@@ -17,7 +16,6 @@ export default function HomePage() {
       <FeaturesSection />
       <Testimonials />
       <Faq />
-      <Pricing />
       <CtaSection />
       <Footer />
     </>
