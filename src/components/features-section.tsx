758971d7e247e3457c70d6108d7d6b4c75bbd91f
James Murdza <james@jamesmurdza.com>
2025-04-10 15:35:55 +0200

    Add course titles

--- src/components/features-section.tsx
+++ src/components/features-section.tsx
@@ -6,26 +6,24 @@ export function Features() {
       <div className="container mx-auto flex flex-col gap-12">
         <div className="flex justify-between w-full flex-col lg:flex-row gap-4 lg:items-end">
           <h2 className="font-heading tracking-tight sm:text-5xl text-3xl text-balance font-semibold text-left flex-1">
-            Made for modern product teams
+            Our courses
           </h2>
           <p className="text-lg text-muted-foreground flex-1">
-            Our analytics platform is built on the strategies and insights that empower leading
-            data-driven teams: deep visibility, real-time intelligence, and a dedication to
-            continuous improvement.
+            We teach free courses to the public, online and in person.
           </p>
         </div>
         <div className="grid gap-8 sm:grid-cols-2 lg:grid-cols-3 grid-cols-1">
           <FeatureCard
             image="/images/filters.png"
-            title="Designed for real-time helpful data insights"
+            title="Machine Learning"
           />
           <FeatureCard
             image="/images/calendar.png"
-            title="Optimized for fast and flexible scheduling"
+            title="AI Agent Camp (Python)"
           />
           <FeatureCard
             image="/images/notification.png"
-            title="Built for seamless team collaboration"
+            title="AI Agent Camp (No code)"
           />
         </div>
       </div>
